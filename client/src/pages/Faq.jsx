e11eed571629835ef4f4153117ec60ef109d518d
rebcs <rebecaciriaca@gmail.com>
2024-11-05 17:51:43 -0300

    login, faq ok

--- client/src/pages/Faq.jsx
+++ client/src/pages/Faq.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import '../assets/styles/Faq.css'
 
 const Faq = () => {
     return (
--- client/src/pages/SobreNos.jsx
+++ client/src/pages/SobreNos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import '../assets/styles/SobreNos.css'
 
 const SobreNos = () => {
     return (
